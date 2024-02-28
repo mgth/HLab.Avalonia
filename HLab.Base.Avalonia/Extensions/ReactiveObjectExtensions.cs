6d4f05ecd68351d55b7ba7505af43cb5c0b11b3f
Mathieu GRENET <sub@mgth.fr>
2024-02-28 10:13:45 +0100

    Avalonia 11.0.9

---
+++ HLab.Base.Avalonia/Extensions/ReactiveObjectExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.CompilerServices;
+using ReactiveUI;
+
+namespace HLab.Base.Avalonia.Extensions
+{
+    public static class ReactiveObjectExtensions
+    {
+        public static TRet LazyGet<TObj, TRet>(
+            this TObj @this,
+            ref TRet backingField, 
+            Func<TRet,bool> predicate,
+            Func<TObj,Task> setter
+            )
+            where TObj : IReactiveObject
+        {
+            if (predicate(backingField))
+            {
+                Task.Run(() => setter(@this));
+            }
+
+            return backingField;
+        }
+
+        public static TRet? LazyGet<TObj, TRet>(
+            this TObj @this,
+            ref TRet? backingField,
+            Func<TObj, Task> setter
+        )
+            where TObj : IReactiveObject
+            where TRet : class
+        {
+            if (backingField == null)
+            {
+                Task.Run(() => setter(@this));
+            }
+            return backingField;
+        }
+
+        public static TRet? LazyGet<TObj, TRet>(
+            this TObj @this,
+            ref TRet? backingField,
+            Action<TObj> setter
+        )
+            where TObj : IReactiveObject
+            where TRet : class
+        {
+            if (backingField == null)
+            {
+                Task.Run(() => setter(@this));
+            }
+            return backingField;
+        }
+
+    }
+}
