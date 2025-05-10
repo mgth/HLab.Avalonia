7ad68a7dcb8c28379708422c0482648f39ff8ec9
Mathieu GRENET <mathieu@mgth.fr>
2025-05-10 12:36:23 +0200

    Refactorisation et mise à jour des fichiers de projet
    Ajout d'une nouvelle classe statique `RectExtensions` dans `RectExtensions.cs` pour faciliter la conversion entre les types de rectangles de HLab et d'Avalonia,

--- HLab.Geo.Avalonia/RectExtensions.cs
+++ HLab.Geo.Avalonia/RectExtensions.cs
@@ -1,6 +1,6 @@
 ﻿namespace HLab.Geo.Avalonia;
 
-public static class Class1
+public static class RectExtensions
 {
    public static global::Avalonia.Rect ToAvalonia(this HLab.Geo.Rect r) => new global::Avalonia.Rect(r.Left, r.Top, r.Width, r.Height);
    public static Rect ToHLab(this global::Avalonia.Rect r) => new(r.X, r.Y, r.Width, r.Height);
